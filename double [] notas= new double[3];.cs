3a4fb2fbff99f4a068b4cb3e09db01bb6fad5bf8
faabiianm123 <112511565+faabiianm123@users.noreply.github.com>
2022-09-06 08:32:12 -0500

    Add files via upload

---
+++ double [] notas= new double[3];.cs
@@ -0,0 +1,15 @@
+double [] notas= new double[3];
+String nombre;
+String carrera; 
+double final; 
+Console.WriteLine("Diga su nombre");
+nombre=Console.ReadLine();
+Console.WriteLine ("Diga su carrera");
+carrera=Console.ReadLine();
+Console.WriteLine(); ("diga su nota 1")
+notas[0]=Console.ReadLine();
+Console.WriteLine();
+notas [1]=convert.ToDouble( console.ReadLine)
+Console.WriteLine("diga su nota 2");
+notas[2]=Convert.ToDouble ( Console.ReadLine()); 
+final= ( notas [0] +notas[1] +notas [2] )/3 
